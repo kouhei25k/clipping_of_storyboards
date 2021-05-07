14b1cabaf7c0e1b5155f6b0fd6a18b4843b7161a
kouhei25k <kouhei25k@gmail.com>
2021-05-08 00:07:47 +0900

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+import glob
 
 
 def draw_text(img: str, text: str, font_size: int, x: int, y: int):
@@ -8,24 +9,33 @@ def draw_text(img: str, text: str, font_size: int, x: int, y: int):
     draw.text((x, y), text, font=font, fill=(0, 0, 0, 255))
 
 
-title_text = "アニメタイトル"
-number_text = "100"
-
-
 def create_Layout(title_text: str, number: int,):
-    croped_img = img.crop((100, 120, 400, 300))
+    X = 100
+    Y = 120
+    HEIGHT = 180
+    LEFT_TOP = 180 * number + Y
+
+    croped_img = img.crop((X, LEFT_TOP, 400, LEFT_TOP+HEIGHT))
     resized_img = croped_img.resize((croped_img.width*6, croped_img.height*6))
     copied_back_img = back_img.copy()
     copied_back_img.paste(resized_img, (100, 300))
 
-    draw_text(copied_back_img, title_text, 40, 500, 130)
-    draw_text(copied_back_img, str(number_text), 40, 1350, 130)
-
-    copied_back_img.save(f'result/result_{number}.png', quality=95)
+    return copied_back_img
 
 
-img = Image.open("images/test.png").convert('RGBA')
-back_img = Image.open("images/back.jpg")
+back_img = Image.open("back_image/back.jpg")
 title_text = "アニメタイトル"
-for i in range(5):
-    create_Layout(title_text, i)
+
+files = glob.glob("images/*.png")
+for index, file in enumerate(files):
+    print(index,file)
+    img = Image.open(file).convert('RGBA')
+
+    for i in range(5):
+
+        number_text = 5*index+(i+1)
+
+        img = create_Layout(title_text, i)
+        draw_text(img, title_text, 40, 500, 130)
+        draw_text(img, str(number_text), 40, 1350, 130)
+        img.save(f'result/{title_text}_{number_text}.png', quality=95)
